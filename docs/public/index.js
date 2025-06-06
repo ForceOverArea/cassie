1c6328e9379ee4e0053219cd2420ddb4b3d90dc7
Grant <christiansengrant18@gmail.com>
2025-06-05 20:15:55 -0500

    fixed pages root directory

---
+++ docs/public/index.js
@@ -0,0 +1,51 @@
+import { init } from './wasm/wasm_wrapper.js';
+import { renderNumeric, renderSymbolic } from './outputRendering.js';
+import { EDITOR, NUMERIC_TOGGLE, SOLN_PANE, SYMBOLIC_TOGGLE } from "./domElements.js";
+
+const SolnShowStates = {
+  SYMBOLIC: "equation",
+  NUMERIC: "maybeValue",
+};
+
+let solnShowState_global = SolnShowStates.SYMBOLIC;
+const wasmImports = {
+  solveSystem() {}
+};
+
+function onInitialization() {
+  trySolveSystem();
+}
+
+// script is deferred, so main function can safely access DOM.
+function main() {
+  EDITOR.oninput = trySolveSystem;
+
+  SYMBOLIC_TOGGLE.onclick = () => {
+    solnShowState_global = SolnShowStates.SYMBOLIC;
+    trySolveSystem();
+  }
+
+  NUMERIC_TOGGLE.onclick = () => {
+    solnShowState_global = SolnShowStates.NUMERIC;
+    trySolveSystem();
+  }
+}
+
+async function trySolveSystem() { 
+  const soln = await wasmImports.solveSystem(EDITOR.innerText);
+  
+  if ('string' === typeof soln) {
+    SOLN_PANE.innerHTML = soln;
+  } else if (SolnShowStates.SYMBOLIC === solnShowState_global) {
+    renderSymbolic(soln);
+  } else {
+    renderNumeric(soln);
+  }
+}
+
+main();
+
+init().then((wasi_reactor) => {
+  Object.assign(wasmImports, wasi_reactor);
+  onInitialization();
+});
---
+++ docs/public/outputRendering.js
@@ -0,0 +1,78 @@
+import { SOLN_PANE } from './domElements.js';
+
+const SYM_SOLN_ID_PREFIX = 'sym-soln-';
+const SYM_SOLN_CLASSLIST = [
+  'sym-soln-entry ',
+  'quick-fill',
+  'clickable',
+].join(' ');
+
+/**
+ * @param {Array} cassieSoln 
+ */
+export function renderSymbolic(cassieSoln) {
+  const symSolns = [];
+
+  // Generate list of equations to add to HTML
+  for (const { equation } of cassieSoln.solution) {
+    symSolns.push(wrapWithSolutionTag(equation, symSolns.length));
+  }
+
+  // Add eqns to HTML
+  SOLN_PANE.innerHTML = renderContext(cassieSoln.context) 
+    + `<br>&nbsp;<br>${solnHeader('Solution:')}<br>`
+    + symSolns.join('<br>');
+
+  // Add onclick events to new tags
+  for (const [i, { steps, symbol }] of cassieSoln.solution.entries()) {
+    const elem = document.getElementById(SYM_SOLN_ID_PREFIX + i);
+    elem.onclick = () => {
+      SOLN_PANE.innerHTML = solnHeader(`Showing steps to solve for ${symbol}:`) 
+        + `<br>${steps}`;
+    }
+  }
+}
+
+/**
+ * @param {Object} cassieSoln 
+ */
+export function renderNumeric(cassieSoln) {
+  const numSolns = [];
+
+  for (const { symbol, maybeValue } of cassieSoln.solution) {
+    numSolns.push(`${symbol} = ${maybeValue}`);
+  }
+
+  SOLN_PANE.innerHTML = renderContext(cassieSoln.context)
+    + `<br>&nbsp;<br>${solnHeader('Solution:')}<br>`
+    + numSolns.join('<br>');
+} 
+
+/**
+ * @param {Object} cassieCtx 
+ */
+function renderContext(cassieCtx) {
+  const constants = [];
+  const functions = [];
+  
+  for (const [symbol, value] of Object.entries(cassieCtx)) {
+    if (value.includes('-&gt;')) {
+      functions.push(`${symbol}${value}`);
+    } else {
+      constants.push(`${symbol} = ${value}`);
+    }
+  }
+
+  const funcText = functions.join('<br>');
+  const cnstText = constants.join('<br>');
+
+  return `${solnHeader('Context:')}<br>${funcText}<br><br>${cnstText}<br>`;
+}
+
+function wrapWithSolutionTag(solutionText, uid) {
+  return `<div id="${SYM_SOLN_ID_PREFIX + uid}" class="${SYM_SOLN_CLASSLIST}">${solutionText}</div>`;
+}
+
+function solnHeader(str) {
+  return `<div class="soln-header">${str}</div>`
+}
