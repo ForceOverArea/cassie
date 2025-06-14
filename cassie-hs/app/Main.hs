7a36e0c747fed746b23095f7664e30ebd0f040cb
Grant <christiansengrant18@gmail.com>
2025-06-14 12:16:49 -0500

    saving progress for CLI feature addition

--- cassie-hs/app/Main.hs
+++ cassie-hs/app/Main.hs
@@ -1,7 +1,23 @@
 module Main where
 
--- import Data.Cassie (solveSystem)
+import Control.Arrow
+import Control.Monad.Reader (asks, runReaderT, ReaderT)
+import Data.Aeson (eitherDecodeStrictText)
+import Data.Cassie (solveSystem)
+-- import Data.Cassie.
+import Data.Text (pack)
+import Settings (parseCassieJSON, CassieJSON(..))
+
+type CassieCLI = ReaderT CassieJSON IO 
 
 main :: IO ()
 main = do
-    putStrLn "hello, world"
+    cassieJSON <- parseCassieJSON <$> readFile "./Cassie.json"
+    entryPointSource <- readFile $ entryPoint cassieJSON
+    return () 
+
+cassieCliMain :: CassieCLI ()
+cassieCliMain = do
+    cassieSourceFilepath <- asks entryPoint
+    let (imports, fnCtx, equations) = parseCassieSource cassieSourceFilepath
+    return "ligma"
---
+++ cassie-hs/app/Settings.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Settings 
+    ( CassieJSON(..)
+    , CassieReportOpts(..)
+    , CassieSolnOpts(..)
+    , parseCassieJSON
+    ) where
+
+import Data.Aeson (eitherDecodeStrictText)
+import Data.Aeson.Types ((.:), withObject, FromJSON(..))
+import qualified Data.Text as Text
+
+data CassieJSON = CassieJSON 
+    { entryPoint :: String
+    , report     :: CassieReportOpts 
+    , solution   :: CassieSolnOpts
+    }
+
+data CassieReportOpts = CassieReportOpts 
+    { title  :: String
+    , author :: String
+    }
+
+data CassieSolnOpts = CassieSolnOpts 
+    { numeric   :: [String] 
+    , constants :: [String] 
+    , symbolic  :: [String] 
+    }
+
+instance FromJSON CassieJSON where
+    parseJSON = withObject "Cassie.json" $ \v -> CassieJSON 
+        <$> v .: "entryPoint"
+        <*> v .: "report"
+        <*> v .: "solution"
+
+instance FromJSON CassieReportOpts where
+    parseJSON = withObject "report options" $ \v -> CassieReportOpts
+        <$> v .: "title"
+        <*> v .: "author"
+
+instance FromJSON CassieSolnOpts where
+    parseJSON = withObject "solution options" $ \v -> CassieSolnOpts
+        <$> v .: "numeric"
+        <*> v .: "constants"
+        <*> v .: "symbolic"
+
+parseCassieJSON :: String -> CassieJSON
+parseCassieJSON = either error id . eitherDecodeStrictText . Text.pack
