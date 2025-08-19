74421c75ae207f619b36d3f0c8dd47b8613cb82b
Grant <christiansengrant18@gmail.com>
2025-08-19 09:31:06 -0500

    committing build script before it can get deleted again

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CABAL_REACTOR_PATH=dist-newstyle/build/wasm32-wasi/ghc-9.12.2.20250327/cassie-cli-0.1.0.0/x/reactor/opt/build/reactor/reactor.wasm
+DIST_REACTOR_DIR=dist/wasi
+SRC_REACTOR_DIR=src/wasi
+
+# Build wasm32-wasi reactor module using GHC/Cabal
+wasm32-wasi-cabal build reactor
+
+# Generate JSFFI source from reactor and create declaration .ts file
+$(wasm32-wasi-ghc --print-libdir)/post-link.mjs -i $CABAL_REACTOR_PATH -o $SRC_REACTOR_DIR/ghc_wasm_jsffi.js
+npx tsc $SRC_REACTOR_DIR/ghc_wasm_jsffi.js --allowJS --declaration --emitDeclarationOnly
+
+# Compile Typescript to JS module (creates 'dist' directory)
+npx tsc
+cp $WASI_REACTOR_DIR $DIST_REACTOR_DIR
