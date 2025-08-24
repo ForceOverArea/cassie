9f83dd9de22b16257d5352801a949e539483a85f
Grant <christiansengrant18@gmail.com>
2025-08-24 10:42:01 -0500

    changed build script

--- build.sh
+++ build.sh
@@ -6,23 +6,24 @@ SRC_REACTOR_DIR=src/wasi
 
 # Get JS + WASM needed for NPM package
 echo "[1/6] Building Haskell project..."
-wasm32-wasi-cabal build reactor >> build_log.txt
+wasm32-wasi-cabal build reactor
 
 echo "[2/6] 'npm install'-ing..."
-npm install >> build_log.txt
+npm install
 
 # Generate JSFFI source from reactor and create declaration .ts file
 echo "[3/6] Generating FFI bindings..."
-$(wasm32-wasi-ghc --print-libdir)/post-link.mjs -i $CABAL_REACTOR_PATH -o $SRC_REACTOR_DIR/ghc_wasm_jsffi.js >> build_log.txt
+mkdir src/wasi
+$(wasm32-wasi-ghc --print-libdir)/post-link.mjs -i $CABAL_REACTOR_PATH -o $SRC_REACTOR_DIR/ghc_wasm_jsffi.js
 
 echo "[4/6] Generating FFI TypeScript declaration..."
-npx tsc $SRC_REACTOR_DIR/ghc_wasm_jsffi.js --allowJS --declaration --emitDeclarationOnly >> build_log.txt
+npx tsc $SRC_REACTOR_DIR/ghc_wasm_jsffi.js --allowJS --declaration --emitDeclarationOnly
 
 # Create  JavaScript build in 'dist' directory
 echo "[5/6] Compiling TypeScript..."
-npx tsc >> build_log.txt
+npx tsc
 
 echo "[6/6] Copying Haskell reactor module to 'dist' directory..."
-cp $CABAL_REACTOR_PATH $DIST_REACTOR_DIR >> build_log.txt
+cp $CABAL_REACTOR_PATH $DIST_REACTOR_DIR
 
 echo "Done!"
